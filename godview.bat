af1311477c96b34b7bb95fe95b4d7c80604dca7d
luosen <1915227519@qq.com>
2023-10-12 18:53:55 +0800

    新增README

--- godview.bat
+++ godview.bat
@@ -6,12 +6,12 @@ set "rootDir=C:\godcmd"
 
 if "%~1" == "-e" (
     set "targetFile=%rootDir%\%2.bat"
-    echo !targetFile!
     if exist "!targetFile!" (
         set "lineCount=0"
         for /F "usebackq delims=" %%L in ("!targetFile!") do (
             set /A "lineCount+=1"
             if !lineCount! == 2 (
+                echo !targetFile!
                 echo 该命令的使用方式为: %%L
                 exit /B
             )
