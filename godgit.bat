bf62e6eb6d2e85c495af34c9c7bf248fa9d8318d
CavenGo <1915227519@qq.com>
2023-10-12 23:38:06 +0800

    优化命令参数

--- godgit.bat
+++ godgit.bat
@@ -7,16 +7,33 @@ setlocal enabledelayedexpansion
 rem 获取命令行参数
 set "command=%~1"
 set "value=%~2"
+set "command1=%~3"
+set "value1=%~4"
 
 if "%command%"== "?" (
-    echo -g 选择上传的分支
+    echo godgit -b branch -m commit  //已绑定远程仓库且已有分支
+    echo godgit -r remote -b branch   //第一次上传
     exit /b 0
 )
 
-if "%command%"== "-g" (	
-    git add .
-    git commit -m "优化"
-    git push -u origin %value%
-    echo 上传完毕
-    exit /b 0
+if "%command%"== "-b" (
+    if "%command1%"== "-m" (	
+        git add .
+        git commit -m "%value1%"
+        git push -u origin %value%
+        exit /b 0
+    )
+)
+
+if "%command%"== "-r" (	
+    if "%command1%"== "-b" (	
+        git init
+        git add .
+        git commit -m "初始化"
+        git branch -M %value1%
+        git remote add
+        git remote add origin %value%
+	git push -u origin %value1%
+        exit /b 0
+    )
 )
